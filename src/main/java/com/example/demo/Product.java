6078c731b58e4af80d7ca59fc426b6b9d0decf75
yadavsudhanshu1824@gmail.com <yadavsudhanshu1824@gmail.com>
2025-05-11 16:11:25 +0530

    multiple param: done

--- src/main/java/com/example/demo/Product.java
+++ src/main/java/com/example/demo/Product.java
@@ -1,13 +1,49 @@
 package com.example.demo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Product {
 
     @Id
-    public int id;
-    public String name;
-    public String description;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+
+    public Product(){}
+    
+    public Product (String _name, String _desc){
+        this.name = _name;
+        this.description = _desc;
+    }
+    public Product setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Product setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return name;
+    }
+
+    public Product setDescription(String desc) {
+        this.description = desc;
+        return this;
+    }
 }
--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -2,16 +2,20 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
+import jakarta.websocket.server.PathParam;
+
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.web.PageableArgumentResolver;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -25,15 +29,39 @@ public class ProductController {
         return "hello world";
     }
 
-    @GetMapping("/all")
+    @GetMapping("/all-paged")
     public List<Product> getPagedData(@RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "5") int size,
             @RequestParam(defaultValue = "id") String col,
-            @RequestParam(defaultValue = "true") boolean asc) {
+            @RequestParam(defaultValue = "false") boolean asc) {
 
         Sort sort = asc ? Sort.by(Direction.ASC, col) : Sort.by(Direction.DESC, col);
         Pageable pageable = PageRequest.of(page, size, sort);
 
         return productRepo.findAll(pageable).toList();
+    }
+
+    @GetMapping("/get/all")
+    public List<Product> getAllData() {
+        return productRepo.findAll();
+    }
+
+    @PostMapping("/save/{id}")
+    public Product save(@PathVariable int id) {
+        Product product = new Product().setName(String.valueOf(id) + "name")
+                .setDescription(String.valueOf(id) + "description");
+        return productRepo.save(product);
+    }
+
+    @GetMapping("/get/{id}")
+    public Product getFromId(@PathVariable int id) {
+        var product = productRepo.getReferenceById(id);
+        return product;
+    }
+
+    @PostMapping("/save")
+    public Product MultiParamTest(@PathParam(value = "name") String name,
+            @PathParam(value = "desc") String desc) {
+        return productRepo.save(new Product().setName(name).setDescription(desc));
     }
 }
